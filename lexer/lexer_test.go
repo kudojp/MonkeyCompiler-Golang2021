72db969cb12cd6569e6db14cfdaa9fb214df455b
kudojp <heyjudejudejude1968@gmail.com>
2021-09-25 21:53:31 +0900

    Fix syntax error in lexer_test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -30,14 +30,14 @@ func TestNextToken(t *testing.T) {
 		if tok.Type != tt.expectedType {
 			t.Fatalf(
 				"tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type
+				i, tt.expectedType, tok.Type,
 			)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf(
 				"tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Literal
+				i, tt.expectedType, tok.Literal,
 			)
 		}
 	}
