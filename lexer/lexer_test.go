181b215ba7da00d0d640e5bd89e16ed34a013f4b
kudojp <heyjudejudejude1968@gmail.com>
2021-09-26 03:33:21 +0900

    Add test for lexing  != ==

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -22,6 +22,9 @@ if (5 < 10){
 } else {
 	return false;
 }
+
+10 == 10;
+10 != 9;
 `
 
 	tests := []struct {
@@ -97,6 +100,16 @@ if (5 < 10){
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.NEQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
