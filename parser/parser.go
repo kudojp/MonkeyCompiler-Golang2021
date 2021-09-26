c906a82c08e3102f9be366f4c6e6e6cd262acfae
kudojp <heyjudejudejude1968@gmail.com>
2021-09-26 17:45:10 +0900

    Add Parser.ParseProgram which can parse only let statements

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
 )
@@ -15,11 +16,76 @@ func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 	p.nextToken()
 	p.nextToken()
-	// At this point, cursor's curToken is the first token in a vien
+	// At this point, cursor's curToken is the first token in a string
 	return p
 }
 
 func (p *Parser) nextToken() {
-	p.curToken = p.peekToken()
+	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken() // move the lexer's position
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	// TODO: add cases of return, if,,,
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil // first token after "let" is not an identifier token.
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil // second token after "let" is not an assign token.
+	}
+
+	// TODO: Set Value of this struct here.
+	// For now, we set nothing for it and proceed until semicolon.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// Helper to check current token's type
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// Helper to check next token's type
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken() // proceed
+		return true
+	} else {
+		return false
+	}
 }
