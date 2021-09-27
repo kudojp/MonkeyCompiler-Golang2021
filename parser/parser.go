3c60d40be56e4b2f7ab240f516129be1325ca73b
kudojp <heyjudejudejude1968@gmail.com>
2021-09-28 01:34:28 +0900

    Add Parser.parseExpression and register it to Parser.prefixParseFns

--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,11 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"strconv"
 )
 
 type Parser struct {
@@ -63,7 +65,9 @@ func New(l *lexer.Lexer) *Parser {
 	// At this point, cursor's curToken is the first token in a string
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
-	p.registerPrefixFn(token.IDENT, p.parseIdentifier) // TODO: register more functions here.
+	p.registerPrefixFn(token.IDENT, p.parseIdentifier)
+	p.registerPrefixFn(token.INT, p.parseIntegerLiteral)
+	// TODO: register more functions here.
 
 	// TODO: define infixParseFns and register functions
 	return p
@@ -76,6 +80,19 @@ func (p *Parser) registerPrefixFn(token token.TokenType, fn prefixParseFn) {
 // Returns an expression node of identifier.
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) nextToken() {
