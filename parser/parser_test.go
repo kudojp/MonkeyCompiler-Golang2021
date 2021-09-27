7554d5157fcfbff768be0eaaba1d46bab1715e94
kudojp <heyjudejudejude1968@gmail.com>
2021-09-28 00:54:14 +0900

    Add parser.TestIdentifierExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -98,3 +98,53 @@ return 993322;
 		}
 	}
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf(
+			"program has not enough statements. got=%d",
+			len(program.Statements),
+		)
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf(
+			"program.Statements[0] is not ast.ExpressionStatement, got=%T",
+			program.Statements[0],
+		)
+	}
+
+	ident := stmt.Expression.(*ast.Identifier)
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("exp not *ast.Identifier. got=%T", ident,)
+	}
+
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s, got=%s", "foobar", ident.Value,)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLIteral not %s. got=%s", "foobar", ident.TokenLiteral(),)
+	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.errors
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
