76bbc0cb2b652929052d91c089f4c52276365941
kudojp <heyjudejudejude1968@gmail.com>
2021-09-28 22:32:52 +0900

    Add Parser.noPrefixParseFnError for clearer error message

--- parser/parser.go
+++ parser/parser.go
@@ -37,9 +37,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type) // これ、消さないといけないかも？
 		return nil
 	}
 	leftExp := prefix()
