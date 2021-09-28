d8091fc7f6963135397abeaf115aa1eb70777f36
kudojp <heyjudejudejude1968@gmail.com>
2021-09-29 00:52:34 +0900

    Update Parser.parseExpression to parse infix expression

--- parser/parser.go
+++ parser/parser.go
@@ -45,11 +45,19 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
-		p.noPrefixParseFnError(p.curToken.Type) // これ、消さないといけないかも？
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
 
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+		p.nextToken()
+		leftExp = infix(leftExp)
+	}
 	return leftExp
 }
 
