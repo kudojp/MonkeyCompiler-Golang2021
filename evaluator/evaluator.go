c30b999a4b0dc12c3404ac5e550e42222cbbdc6a
kudojp <heyjudejudejude1968@gmail.com>
2021-10-02 19:59:27 +0900

    Add evaluator.Eval and evaluator.evalStatements

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+// TODO: This method returns the object of the last statement of the given statements.
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
