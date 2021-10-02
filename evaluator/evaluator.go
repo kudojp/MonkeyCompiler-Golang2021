f7c8af95693f3c7390d9da3722fb473117cfff7f
kudojp <heyjudejudejude1968@gmail.com>
2021-10-02 21:56:34 +0900

    Enable evaluator.Eval to evaluate MinusPrefixExpression such as -1

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -50,7 +50,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
-	// TODO: add a case when "-"
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -67,4 +68,13 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
