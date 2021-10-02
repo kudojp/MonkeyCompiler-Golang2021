765543deed089e9752949747d01f0f48f1ff04a3
kudojp <heyjudejudejude1968@gmail.com>
2021-10-02 22:43:33 +0900

    Enable evaluator to parse <>==!= by adding the cases in evaluator.evalIntegerInfixExpression()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -106,6 +106,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
