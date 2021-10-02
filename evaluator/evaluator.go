4da8ccc18e005fa943731742178fae19cc0db854
kudojp <heyjudejudejude1968@gmail.com>
2021-10-03 00:33:49 +0900

    Update evaluator.evalInfixExpression to compare not integers

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -87,7 +87,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
-	// TODO: case like `true == false` should return false here.
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
