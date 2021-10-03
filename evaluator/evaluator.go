cadecf0a6a18ccc14687dbb0b1e27ba6f9999a39
kudojp <heyjudejudejude1968@gmail.com>
2021-10-03 18:39:38 +0900

    Return error object in evaluator.evalProgram, evalBlockStatement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -169,8 +169,11 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	for _, stmt := range block.Statements {
 		result = Eval(stmt)
 
-		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
-			return result
+		if result != nil {
+			rt := result.Type()
+			if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
+				return result
+			}
 		}
 	}
 	return result
