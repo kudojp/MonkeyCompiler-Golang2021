3ec2d3ff67d6e6840c4fb982ea2c3dfec55f5f13
kudojp <heyjudejudejude1968@gmail.com>
2021-10-05 23:11:18 +0900

    Add evaluator.TestFunctionApplication

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -222,7 +222,7 @@ func TestFunctionObject(t *testing.T) {
 	}
 
 	if len(fn.Parameters) != 1 {
-		t.Fatalf("function has wrong parameters. Parameters=%+v", evaluated, evaluated)
+		t.Fatalf("function has wrong parameters. Parameters=%+v", evaluated)
 	}
 
 	if fn.Parameters[0].String() != "x" {
@@ -232,6 +232,25 @@ func TestFunctionObject(t *testing.T) {
 	expectedBody := "(x + 2)"
 	if fn.Body.String() != expectedBody {
 		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x){x;}; identity(5);", 5},
+		{"let identity = fn(x){return x}; identity(5);", 5},
+		{"let double = fn(x) { x + 2; }; double(5);", 10},
+		{"let add = fn(x, y) {x + y;}; add(5, 5);", 10},
+		{"let add = fn(x, y) {x + y;}; add(5+5, add(5,5);", 20},
+		{"let add = fn(x, y) {x + y;}; add(5+5, add(5+5));", 20},
+		{"fn(x){x;}(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
 
