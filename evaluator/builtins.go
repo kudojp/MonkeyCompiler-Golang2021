e471dc0f1216c0ad190bfbf753534d99ec79e416
kudojp <heyjudejudejude1968@gmail.com>
2021-10-07 02:52:02 +0900

    Add a skelton of evaluator.builtins with "len"

---
+++ evaluator/builtins.go
@@ -0,0 +1,13 @@
+package evaluator
+
+import "monkey/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object{
+			return NULL // TODO
+		},
+	},
+}
+
+
