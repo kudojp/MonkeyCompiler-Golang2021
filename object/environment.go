39f265cbd6a7874227478f30b553a83d4925862d
kudojp <heyjudejudejude1968@gmail.com>
2021-10-06 19:25:23 +0900

    Extend object.env to hold reference to outer env

--- object/environment.go
+++ object/environment.go
@@ -1,16 +1,28 @@
 package object
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
-}
-
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+
 	return obj, ok
 }
 
