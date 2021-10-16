6932b53fa78f590c5834e010273ac4635337b2ac
kudojp <heyjudejudejude1968@gmail.com>
2021-10-16 21:37:54 +0900

    Add test of lexing hash

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,6 +26,7 @@ if (5 < 10){
 10 == 10;
 10 != 9;
 [1, 2];
+{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -118,6 +119,11 @@ if (5 < 10){
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
