29a7a1d01e8bcc9ea64782c8440b5719e41aa435
kudojp <heyjudejudejude1968@gmail.com>
2021-10-17 01:58:17 +0900

    Add evaluator.TestHashIndexExpression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -169,6 +169,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`{"name": "Monkey"}[fn(x){ x }]`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 
 	for _, tt := range tests {
@@ -409,7 +413,52 @@ func TestHashLiterals(t *testing.T) {
 		}
 		testIntegerObject(t, pair.Value, expectedVal)
 	}
+}
 
+func TestHashIndexExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"]`,
+			5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil,
+		},
+		{
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
 }
 
 func testEval(input string) object.Object {
