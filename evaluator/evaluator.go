118141842761f740473448dd4e69f9d6cce6cc5f
kudojp <heyjudejudejude1968@gmail.com>
2021-10-16 18:48:01 +0900

    Enable our evaluator to evaluate ast.IndexExpression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -87,8 +87,17 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
-
 	return nil
 }
 
@@ -295,6 +304,26 @@ func unwrapReturnValue(obj object.Object) object.Object {
 
 func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || max < idx {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
 
 func isError(obj object.Object) bool {
