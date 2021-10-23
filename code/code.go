b63169d852e217ab7763f2368c4f3d3cf83b3b3c
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 23:34:59 +0900

    Add a case when operandCount=0 in  code.fmtInstruction

--- code/code.go
+++ code/code.go
@@ -38,6 +38,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 	}
 
 	switch operandCount {
+	case 0:
+		return fmt.Sprintf(def.Name)
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
