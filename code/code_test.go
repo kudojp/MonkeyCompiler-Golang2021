43387e5d53ab11d3f78b1cb3cea0e39bcb9cd5bb
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 23:34:01 +0900

    Update TestInstructionsString to test stringifying OpAdde instruction

--- code/code_test.go
+++ code/code_test.go
@@ -35,14 +35,14 @@ func TestMake(t *testing.T) {
 
 func TestInstructionsString(t *testing.T) {
 	instructions := []Instructions{
-		Make(OpConstant, 1),
+		Make(OpAdd),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
 
-	expected := `0000 OpConstant 1
-0003 OpConstant 2
-0006 OpConstant 65535
+	expected := `0000 OpAdd
+0001 OpConstant 2
+0004 OpConstant 65535
 `
 
 	concatted := Instructions{} // array of byte arrays
