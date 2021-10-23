b0d88dab71f493c278b764cee774f9c537003129
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 23:47:14 +0900

    Modify compiler.TestIntegerArithmetic to expect an OpAdd instruction

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -23,6 +23,7 @@ func TestIntegerArithmetic(t *testing.T) {
 			expectedInstructions: []code.Instructions{
 				code.Make(code.OpConstant, 0),
 				code.Make(code.OpConstant, 1),
+				code.Make(code.OpAdd),
 			},
 			expectedConstants: []interface{}{1, 2},
 		},
