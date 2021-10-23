e9266ca7220dc39da2e1c6456513fb15fb78b9a4
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 13:45:24 +0900

    Add code.Lookup() to Lookup definition from a byte of opcode

--- code/code.go
+++ code/code.go
@@ -1,5 +1,7 @@
 package code
 
+import "fmt"
+
 type Instructions []byte
 type Opcode byte
 
@@ -14,4 +16,13 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}}, // Thus, up to 65536 constants could be defied.
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+
+	return def, nil
 }
