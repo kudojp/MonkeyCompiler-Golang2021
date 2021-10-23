d059d5a260b465ff3b67e4dac2597b61e846eb83
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 13:44:38 +0900

    Add code.Opconstant along with its definition

--- code/code.go
+++ code/code.go
@@ -2,3 +2,16 @@ package code
 
 type Instructions []byte
 type Opcode byte
+
+const (
+	OpConstant Opcode = iota
+)
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}}, // Thus, up to 65536 constants could be defied.
+}
