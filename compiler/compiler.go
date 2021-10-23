f523ffd0d9ca0d7ae89602c99aff94c4ec4e31e0
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 14:25:48 +0900

    Add compiler.Compiler with empty method #Compile

---
+++ compiler/compiler.go
@@ -0,0 +1,23 @@
+package compiler
+
+import (
+	"monkey/ast"
+	"monkey/code"
+	"monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil // TODO
+}
