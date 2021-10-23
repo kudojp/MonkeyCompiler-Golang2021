a90a34b1bfe8cf808f50d927f2c8e6cbf3e33c4a
kudojp <heyjudejudejude1968@gmail.com>
2021-10-23 14:28:46 +0900

    Add Compiler#Bytecode() which returns Bytecode struct of the compiler

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -21,3 +21,15 @@ func New() *Compiler {
 func (c *Compiler) Compile(node ast.Node) error {
 	return nil // TODO
 }
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func (c *Compiler) ByteCode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
