8fc7cd5762093d574ea4227038a5f0b09b9a8101
kudojp <heyjudejudejude1968@gmail.com>
2021-10-24 14:55:08 +0900

    Add cases to test infix comparisons to vm.TestBooleanExpressions

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -32,6 +32,18 @@ func TestBooleanExpressions(t *testing.T) {
 	tests := []vmTestCase{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
 	}
 	runVmTests(t, tests)
 }
