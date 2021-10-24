3bd10a0cbf6b4975d58dfe194fd5bcff24ea1112
kudojp <heyjudejudejude1968@gmail.com>
2021-10-24 11:07:04 +0900

    Add OpSub, OpMul, OpDiv along with their definitions

--- code/code.go
+++ code/code.go
@@ -52,6 +52,9 @@ type Opcode byte
 const (
 	OpConstant Opcode = iota
 	OpAdd
+	OpSub
+	OpMul
+	OpDiv
 	OpPop
 )
 
@@ -63,6 +66,9 @@ type Definition struct {
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}}, // Thus, up to 65536 constants could be defied.
 	OpAdd:      {"OpAdd", []int{}},
+	OpSub:      {"OpSub", []int{}},
+	OpMul:      {"OpMul", []int{}},
+	OpDiv:      {"OpDiv", []int{}},
 	OpPop:      {"OpPop", []int{}},
 }
 
