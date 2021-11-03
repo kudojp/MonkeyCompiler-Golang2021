6d5d793e8262f196965deb5f67ed3edf4c17a7d0
kudojp <heyjudejudejude1968@gmail.com>
2021-11-03 19:18:41 +0900

    Add new opcode: OpNull

--- code/code.go
+++ code/code.go
@@ -26,6 +26,7 @@ const (
 	OpPop
 	OpJumpNotTruthy
 	OpJump
+	OpNull
 )
 
 type Definition struct {
@@ -49,6 +50,7 @@ var definitions = map[Opcode]*Definition{
 	OpPop:           {"OpPop", []int{}},
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
+	OpNull:          {"OpNull", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
