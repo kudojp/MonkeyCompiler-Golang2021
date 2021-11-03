8fc0dbcd2a3e13357134ee45e04ddbb62304ca5b
kudojp <heyjudejudejude1968@gmail.com>
2021-11-04 00:10:18 +0900

    Add compiler.TestDefine

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -298,6 +298,24 @@ func TestGlobalLetStatements(t *testing.T) {
 	runCompilerTest(t, tests)
 }
 
+func TestDefine(t *testing.T) {
+	expected := map[string]Symbol{
+		"a": Symbol{Name: "a", Scope: GlobalScope, Index: 0},
+		"b": Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+	}
+	global := NewSymbolTable()
+
+	a := global.Define("a")
+	if a != expected["a"] {
+		t.Errorf("expected a=%+v, got=%+v", expected["a"], a)
+	}
+
+	b := global.Define("b")
+	if b != expected["b"] {
+		t.Errorf("expected b=%+v, got=%+v", expected["b"], b)
+	}
+}
+
 func runCompilerTest(t *testing.T, tests []compilerTestCase) {
 	t.Helper()
 
