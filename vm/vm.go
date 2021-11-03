f79b2956e0e1e18988209fedc5c44653834cb03d
kudojp <heyjudejudejude1968@gmail.com>
2021-11-03 22:37:27 +0900

    Update vm to treat Null as false

--- vm/vm.go
+++ vm/vm.go
@@ -235,6 +235,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
