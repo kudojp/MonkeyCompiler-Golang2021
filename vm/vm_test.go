f3c6a57abeee2f4c1bdfb35b3e5b79a2b027138e
kudojp <heyjudejudejude1968@gmail.com>
2021-11-03 18:28:10 +0900

    Add vm.TestConditionals

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -55,6 +55,22 @@ func TestBooleanExpressions(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestConditionals(t *testing.T) {
+	tests := []vmTestCase{
+		{"if (true) { 10 }", 10},
+		{"if (true) { 10 } else { 20 }", 10},
+		{"if (false) { 10 } else { 20 }", 20},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		// TODO: Add later
+		// {"if (1 > 2) { 10 }", Null},
+		// {"if (false) { 10 }", Null},
+	}
+	runVmTests(t, tests)
+}
+
 /*
 Tests the top element in the stack.
 */
