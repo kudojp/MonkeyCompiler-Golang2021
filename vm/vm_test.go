03edb128f0def43b534b5d5cf8d8e331062d7884
kudojp <heyjudejudejude1968@gmail.com>
2021-11-03 19:15:41 +0900

    Add test cases to TestConditionals

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -64,9 +64,8 @@ func TestConditionals(t *testing.T) {
 		{"if (1 < 2) { 10 }", 10},
 		{"if (1 < 2) { 10 } else { 20 }", 10},
 		{"if (1 > 2) { 10 } else { 20 }", 20},
-		// TODO: Add later
-		// {"if (1 > 2) { 10 }", Null},
-		// {"if (false) { 10 }", Null},
+		{"if (1 > 2) { 10 }", Null},
+		{"if (false) { 10 }", Null},
 	}
 	runVmTests(t, tests)
 }
@@ -115,6 +114,10 @@ func testExpectedObject(t *testing.T, expected interface{}, actual object.Object
 		err := testBooleanObject(expected, actual)
 		if err != nil {
 			t.Errorf("testBooleanObject failed: %s", err)
+		}
+	case *object.Null:
+		if actual != Null {
+			t.Errorf("object is not Null: %T (%+v)", actual, actual)
 		}
 	}
 }
