06a4ca380ae3ec62318cbb5a253c424f5b98b4ba
kudojp <heyjudejudejude1968@gmail.com>
2021-11-03 20:14:04 +0900

    Update compiler.TestConditionals to use artificial alternatives

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -172,7 +172,7 @@ func TestBooleanExpressions(t *testing.T) {
 	runCompilerTest(t, tests)
 }
 
-func TestConditionals(t *testing.T){
+func TestConditionals(t *testing.T) {
 	tests := []compilerTestCase{
 		{
 			input: `
@@ -183,14 +183,18 @@ func TestConditionals(t *testing.T){
 				// 0000
 				code.Make(code.OpTrue),
 				// 0001
-				code.Make(code.OpJumpNotTruthy, 7),
+				code.Make(code.OpJumpNotTruthy, 10),
 				// 0004
 				code.Make(code.OpConstant, 0),
 				// 0007
-				code.Make(code.OpPop),
-				// 0008
-				code.Make(code.OpConstant, 1),
+				code.Make(code.OpJump, 11),
+				// 0010
+				code.Make(code.OpNull),
 				// 0011
+				code.Make(code.OpPop),
+				// 0012
+				code.Make(code.OpConstant, 1),
+				// 0015
 				code.Make(code.OpPop),
 			},
 		},
@@ -216,7 +220,30 @@ func TestConditionals(t *testing.T){
 				code.Make(code.OpConstant, 2),
 				// 0017
 				code.Make(code.OpPop),
-
+			},
+		},
+		{
+			input: `
+			if (true) { 10 }; 3333
+			`,
+			expectedConstants: []interface{}{10, 3333},
+			expectedInstructions: []code.Instructions{
+				// 0000
+				code.Make(code.OpTrue),
+				// 0001
+				code.Make(code.OpJumpNotTruthy, 10),
+				// 0004
+				code.Make(code.OpConstant, 0),
+				// 0007
+				code.Make(code.OpJump, 11),
+				// 0010
+				code.Make(code.OpNull),
+				// 0011
+				code.Make(code.OpPop),
+				// 0012
+				code.Make(code.OpConstant, 1),
+				// 0015
+				code.Make(code.OpPop),
 			},
 		},
 	}
