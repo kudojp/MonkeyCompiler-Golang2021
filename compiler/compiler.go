a4e25ed8fd1b019deed4c09fc650e8f45dd96c54
kudojp <heyjudejudejude1968@gmail.com>
2021-11-13 03:08:29 +0900

    Add compiler.NewWithState which is used for REPL

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -30,6 +30,14 @@ func New() *Compiler {
 	}
 }
 
+// Used for REPL.
+func NewWithState(s *SymbolTable, constants []object.Object) *Compiler{
+	compiler := New()
+	compiler.symbolTable = s
+	compiler.constants = constants
+	return compiler
+}
+
 func (c *Compiler) Compile(node ast.Node) error {
 	switch node := node.(type) {
 	case *ast.Program:
