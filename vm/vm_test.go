1625a4b39194a3a1f077a29d82350c89bf6284d4
kudojp <heyjudejudejude1968@gmail.com>
2021-11-13 02:18:51 +0900

    Add compiler.TestGlobalLetStatements()

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -73,6 +73,15 @@ func TestConditionals(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestGlobalLetStatements(t *testing.T) {
+	tests := []vmTestCase{
+		{"let one = 1; one", 1},
+		{"let one = 1; let two = 2; one + two", 3},
+		{"let one = 1; let two = one + one; one + two", 3},
+	}
+	runVmTests(t, tests)
+}
+
 /*
 Tests the top element in the stack.
 */
