c642db3bb8c2ec25ca6e1a6498ef3ef928ac330d
kudojp <heyjudejudejude1968@gmail.com>
2021-11-13 14:37:28 +0900

    Enable the vm to understand an OpHash instruction

--- vm/vm.go
+++ vm/vm.go
@@ -116,17 +116,48 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpHash:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			hash, err := vm.buildHash(vm.sp-numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+			vm.sp = vm.sp - numElements
+			err = vm.push(hash)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func (vm *VM) buildArray(startIndex, endIndex int) object.Object{
-	elements := make([]object.Object, endIndex - startIndex)
-	for i := startIndex; i < endIndex; i++{
+func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
+	elements := make([]object.Object, endIndex-startIndex)
+	for i := startIndex; i < endIndex; i++ {
 		elements[i-startIndex] = vm.stack[i]
 	}
 	return &object.Array{Elements: elements}
+}
+
+func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
+	hashedPairs := make(map[object.HashKey]object.HashPair)
+
+	for i := startIndex; i < endIndex; i += 2 {
+		key := vm.stack[i]
+		value := vm.stack[i+1]
+		pair := object.HashPair{Key: key, Value: value}
+
+		hashableKey, ok := key.(object.Hashable)
+		if !ok {
+			return nil, fmt.Errorf("unusable as a hash key: %s", key.Type())
+		}
+
+		hashedPairs[hashableKey.HashKey()] = pair
+	}
+	return &object.Hash{Pairs: hashedPairs}, nil
 }
 
 func (vm *VM) push(o object.Object) error {
