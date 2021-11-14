60ceca1455ce6f83330596f43d03aba013849b14
kudojp <heyjudejudejude1968@gmail.com>
2021-11-15 00:10:05 +0900

    Add test cases to vm.TestCallingFunctionsWithoutArguments()

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -154,6 +154,23 @@ func TestCallingFunctionsWithoutArguments(t *testing.T) {
 			`,
 			expected: 15,
 		},
+		{
+			input: `
+			let one = fn(){1;};
+			let two = fn(){2;};
+			one() + two()
+			`,
+			expected: 3,
+		},
+		{
+			input: `
+			let a = fn(){1};
+			let b = fn(){a()+1};
+			let c = fn(){b()+1};
+			c();
+			`,
+			expected: 3,
+		},
 	}
 	runVmTests(t, tests)
 }
