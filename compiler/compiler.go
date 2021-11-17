5a823aa4d295bc950277f784af56dab9399b5a40
kudojp <heyjudejudejude1968@gmail.com>
2021-11-17 18:36:12 +0900

    Set NumLocals of CompiledFunction when leaving the scope

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -267,10 +267,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !c.lastInstructionIs(code.OpReturnValue) {
 			c.emit(code.OpReturn)
 		}
-
+		numLocals := c.symbolTable.numDefinitions
 		instructions := c.leaveScope()
 
-		compiledFn := &object.CompiledFunction{Instructions: instructions}
+		compiledFn := &object.CompiledFunction{
+			Instructions: instructions,
+			NumLocals:    numLocals,
+		}
 		c.emit(code.OpConstant, c.addConstant(compiledFn))
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
