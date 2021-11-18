e7bcf163b9149e02b4b1863edc519d056556a0e7
kudojp <heyjudejudejude1968@gmail.com>
2021-11-18 23:27:34 +0900

    Add vm.TestCallingFunctionsWithArgumentsAndBindings()

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -272,6 +272,26 @@ func TestCallingFunctionsWithBindings(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestCallingFunctionsWithArgumentsAndBindings(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let identity = fn(a){ a; }
+			identity(4)
+			`,
+			expected: 4,
+		},
+		{
+			input: `
+			let sum = fn(a, b){ a + b; };
+			sum(1, 2);
+			`,
+			expected: 3,
+		},
+	}
+	runVmTests(t, tests)
+}
+
 func TestFirstClassFunctions(t *testing.T) {
 	tests := []vmTestCase{
 		{
