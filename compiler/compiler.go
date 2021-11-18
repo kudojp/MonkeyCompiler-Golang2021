c2a59b103a2c170a82dbfe19b744212680fa084b
kudojp <heyjudejudejude1968@gmail.com>
2021-11-18 22:30:25 +0900

    Emit arguments as instaructions when compiling a CallExpression object

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -286,7 +286,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
-		c.emit(code.OpCall)
+
+		for _, a := range node.Arguments {
+			err := c.Compile(a)
+			if err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpCall, len(node.Arguments))
 	}
 	return nil
 }
