fca62fe2718aff9b38d4e1aeb1269b5a5cb9a46d
kudojp <heyjudejudejude1968@gmail.com>
2021-11-19 00:12:46 +0900

    Add a test case to vm.TestCallingFunctionsWithArgumentsAndBindings()

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -288,6 +288,16 @@ func TestCallingFunctionsWithArgumentsAndBindings(t *testing.T) {
 			`,
 			expected: 3,
 		},
+		{
+			input: `
+			let globalNum = 10;
+
+			let sum = fn(a, b){ let c = a + b; c + globalNum; };
+			let outer = fn() { sum(1, 2) + sum(3, 4) + globalNum; };
+			outer() + globalNum;
+			`,
+			expected: 50,
+		},
 	}
 	runVmTests(t, tests)
 }
