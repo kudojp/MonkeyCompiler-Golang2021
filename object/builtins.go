df02679b0cea78c08b3d7d211f4022a6923948d4
kudojp <heyjudejudejude1968@gmail.com>
2021-11-20 00:49:28 +0900

    Move fn `push` from evaluator.builtins to object.Builtins

--- object/builtins.go
+++ object/builtins.go
@@ -94,6 +94,26 @@ var Builtins = []struct {
 			return nil
 		}},
 	},
+	{
+		"push",
+		&Builtin{Fn: func(args ...Object) Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got=%s", args[0].Type())
+			}
+
+			arr := args[0].(*Array)
+			length := len(arr.Elements)
+
+			newElements := make([]Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &Array{Elements: newElements}
+		}},
+	},
 }
 
 func newError(format string, a ...interface{}) *Error {
