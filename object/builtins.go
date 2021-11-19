eb58f0fc478e3c9556fb974a2285133dd70283e3
kudojp <heyjudejudejude1968@gmail.com>
2021-11-19 22:12:22 +0900

    Define builtin function `len` in object package (which is now duplicated with the one in evaluator package)

---
+++ object/builtins.go
@@ -0,0 +1,29 @@
+package object
+
+import "fmt"
+
+var Builtins = []struct {
+	Name    string
+	Builtin *Builtin
+}{
+	{
+		"len",
+		&Builtin{Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			switch arg := args[0].(type) {
+			case *Array:
+				return &Integer{Value: int64(len(arg.Elements))}
+			case *String:
+				return &Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported. got=%s", args[0].Type())
+			}
+		}},
+	},
+}
+
+func newError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
