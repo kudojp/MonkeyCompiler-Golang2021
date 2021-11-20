3aff2bc383d8e5af18b495717751d2c3809b28c1
kudojp <heyjudejudejude1968@gmail.com>
2021-11-21 01:46:07 +0900

    Update Instructions#fmtInstruction() to print instructions with 2 operands

--- code/code.go
+++ code/code.go
@@ -122,6 +122,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 		return fmt.Sprintf(def.Name)
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	case 2:
+		return fmt.Sprintf("%s %d %d", def.Name, operands[0], operands[1])
 	}
 
 	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
