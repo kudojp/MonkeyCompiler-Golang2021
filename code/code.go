80c0c38001d3e57ea4d36d91a38d231c7e7e0816
kudojp <heyjudejudejude1968@gmail.com>
2021-11-21 13:19:15 +0900

    Define new opcode: OpGetFree

--- code/code.go
+++ code/code.go
@@ -39,6 +39,7 @@ const (
 	OpSetLocal
 	OpGetBuiltin
 	OpClosure
+	OpGetFree
 )
 
 type Definition struct {
@@ -75,6 +76,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetLocal:      {"OpSetLocal", []int{1}},
 	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
 	OpClosure:       {"OpClosure", []int{2, 1}}, // (index of its function in the constant pool, number of free variables)
+	OpGetFree:       {"OpGetFree", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
