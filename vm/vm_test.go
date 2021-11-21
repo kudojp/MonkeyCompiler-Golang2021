9d5bf0f22cc6658198c1b1ed0e17f20c0f53ad4f
kudojp <heyjudejudejude1968@gmail.com>
2021-11-21 19:21:40 +0900

    Add vm.TestClosures()

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -385,6 +385,19 @@ func TestBuiltinFunctions(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestClosures(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let newClosure = fn(a){ fn(){a;}; }
+			let closure = newClosure(99)
+			closure()`,
+			expected: 99,
+		},
+	}
+	runVmTests(t, tests)
+}
+
 /*
 Tests the top element in the stack.
 */
