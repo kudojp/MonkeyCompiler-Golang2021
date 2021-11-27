5a875e937703a9e50295642cb25b33104e589afb
kudojp <heyjudejudejude1968@gmail.com>
2021-11-27 15:05:56 +0900

    Fix evalutor.unwrapReturnValue()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -319,7 +319,7 @@ func extendFunctionEnv(
 
 func unwrapReturnValue(obj object.Object) object.Object {
 	if returnValue, ok := obj.(*object.ReturnValue); ok {
-		return returnValue
+		return returnValue.Value
 	}
 	return obj
 }
